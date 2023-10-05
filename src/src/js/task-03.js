846a4c0088b21b891288e659b6bac192a3c0fe02
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-10-05 11:16:18 +0300

    3 завдання

--- src/src/js/task-03.js
+++ src/src/js/task-03.js
@@ -12,3 +12,14 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+//достукуємося до ul
+const container = document.querySelector('.gallery');
+//створюємо розмітку. Спочатку за допомогою map перебираємо масив images, потім деструктуризуємо url і alt, щоб ми могли легше їх використовувати. Робимо за доп шаблонного рядка. map повертає масив, треьа привести до рядка, щоб використати insertAdjacentHTML
+const marcup = images.map(({ url, alt }) =>
+  `<li>
+  <img src="${url}" alt="${alt}" class ="item-images" width="400" height="400">
+   </li>`
+).join('');
+console.dir(marcup);
+//додаємо в DOM
+container.insertAdjacentHTML('beforeend', marcup);
