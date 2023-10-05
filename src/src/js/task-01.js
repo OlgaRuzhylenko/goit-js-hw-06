a5074477fd00070c6cabaaebc430a15fe317dd94
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-10-05 10:13:46 +0300

    Update task-01.js

--- src/src/js/task-01.js
+++ src/src/js/task-01.js
@@ -5,20 +5,18 @@ const list = document.querySelector("#categories");
 // console.dir(list.textContent);
 
 //знаходження і виведення всіх дітей - лішок списку
-const allListItem = list.children;
-// console.dir(allListItem);
-//використали спред
-// console.log([...list.children])
+const itemsOfMainList = list.children;
+// console.dir(itemsOfMainList);
 
-const numberOfItem = `Number of categories: ${allListItem.length}`;
-console.dir(numberOfItem);
+const numberOfСategories = `Number of categories: ${itemsOfMainList.length}`;
+console.dir(numberOfСategories);
 
-
+// робимо спред і перебираємо масив. Виводимо текстовий контент першого леемента li (це h2) і тут же достукуємося до останнього елемента li (це ul) і рахуємо його довжину
 [...list.children].forEach((item) => {
-    const itemNameOfTitle = item.firstElementChild.textContent;
-console.dir(`Category: ${itemNameOfTitle}`);
+    const titleOfCategories = item.firstElementChild.textContent;
+    console.dir(`Category: ${titleOfCategories}`);
+    const numberOfCategoreItems = item.lastElementChild.children.length;
+console.dir(`Elements: ${numberOfCategoreItems}`);
 });
-
-
 
 
