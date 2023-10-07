c16dbf755d1150bd35534d20190e419545cac765
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-10-07 07:24:40 +0300

    2 перероблене

--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -9,26 +9,16 @@ const ingredients = [
 //Достукуємось до List
 const list = document.querySelector('#ingredients');
 console.log(list);
-//створюємо кожну li і додаємо їй текстовий контент
-const li = document.createElement('li');
-li.textContent = 'Potatoes';
-li.classList.add('item')
-console.log(li);
-const li2 = document.createElement('li');
-li2.textContent =  'Mushrooms';
-li2.classList.add('item')
-console.log(li2);
-const li3 = document.createElement('li');
-li3.textContent = 'Garlic';
-li3.classList.add('item')
-console.log(li3);
-const li4 = document.createElement('li');
-li4.textContent = 'Herbs';
-li4.classList.add('item')
-console.log(li4);
-const li5 = document.createElement('li');
-li5.textContent = 'Condiments';
-li5.classList.add('item')
-console.log(li5);
+
+//перебираємо за доп map вказаний масив ingredients
+const allIngredients = ingredients.map((ingredient) => {
+  //створюємо собі всі li
+  const li = document.createElement('li');
+  li.textContent = ingredient;
+  li.classList.add('item');
+  console.log(li);
+  //якщо не повернемо, то буде undef
+  return li;
+})
 //додаємо li в list в DOM
-list.append(li, li2, li3, li4, li5);
+list.append(...allIngredients);
