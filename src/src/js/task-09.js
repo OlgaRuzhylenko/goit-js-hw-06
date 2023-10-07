f53296850e16be94bb9b1479eb2f42d856ace755
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-10-07 11:07:10 +0300

    9 завдання

--- src/src/js/task-09.js
+++ src/src/js/task-09.js
@@ -3,3 +3,24 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+//достукуємося до всіх елементів
+const container = document.querySelector('.widget');
+console.dir(container);
+const button = document.querySelector('.change-color');
+console.dir(button);
+const span = document.querySelector('.color');
+console.dir(span);
+
+//додажмо до кнопки слухача подій
+button.addEventListener('click', onClick);
+
+
+function onClick (event) {
+  //викликаємо функцію рандомного кольору і присвоюємо у змінну
+  const someColor = getRandomHexColor();
+  //додаємо цей колір на контейнер
+  container.style.backgroundColor = someColor;
+  // console.log('Випадковий колір:', someColor);
+  //виводимо у спан цей випадковий колір
+  console.log(span.textContent = someColor); 
+}ж
