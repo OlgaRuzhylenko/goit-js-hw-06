24106da6afeb5ad805b86d932928d5afc76f1d90
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-10-07 17:51:38 +0300

    10 почала

--- src/src/js/task-10.js
+++ src/src/js/task-10.js
@@ -3,3 +3,41 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const container = document.querySelector('#controls');
+console.dir(container);
+
+const userInput = container.querySelector('input');
+console.dir(userInput);
+
+const btnCreate = container.querySelector('[data-create]');
+console.dir(btnCreate);
+
+const btnDestroy = container.querySelector('[data-destroy]'); ;
+console.dir(btnDestroy);
+
+btnCreate.addEventListener("click", onClickAddDiv);
+
+function onClickAddDiv (event) {
+  const inputValue = userInput.value;
+  console.dir(inputValue);
+
+  for (let i = 0; i < inputValue; i++) {
+    const newDiv = document.createElement('div');
+    
+    const someColor = getRandomHexColor();
+    newDiv.style.backgroundColor  = someColor;
+    newDiv.style.width = '30px';
+    newDiv.style.height = '30px';
+
+    document.body.appendChild(newDiv);
+    
+  }
+
+};
+
+btnDestroy.addEventListener("click", destroyBoxes);
+
+function destroyBoxes (event) {
+  newDiv.remove();
+}
